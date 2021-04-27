0771911b374312c5d82c015824dea09e400e1e9e
sylvME <81182859+sylvME@users.noreply.github.com>
2021-04-27 12:02:39 -0800

    google font commit

--- inc/enqueues.php
+++ inc/enqueues.php
@@ -20,9 +20,10 @@ if (!(is_admin() )) {
 function prelude_theme_scripts() {
   // CSS
   wp_enqueue_style('prelude-css', get_template_directory_uri() . '/assets/css/theme.min.css', array(), THEME_VERSION );
-  wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&family=PT+Serif:ital,wght@0,700;1,400;1,700&display=swap', array(), THEME_VERSION );
+  wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css?family=PT+Sans|PT+Serif&display=swap' , array(), THEME_VERSION );
   wp_enqueue_style('aos-css', get_template_directory_uri() . '/assets/css/aos.min.css', array(), THEME_VERSION );
   wp_enqueue_style('remodal-css', get_stylesheet_directory_uri() . '/assets/css/remodal.min.css', array(), THEME_VERSION );
+  //'https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&family=PT+Serif:ital,wght@0,700;1,400;1,700&display=swap'
 
   // JS
   wp_enqueue_script('prelude-js', get_template_directory_uri() . '/assets/js/theme.min.js', array('jquery'), THEME_VERSION, true );
